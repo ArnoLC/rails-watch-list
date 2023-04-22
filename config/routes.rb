4d818a70274e26c2e23b579dbc64b380846ea299
arnaud lecorvaisier <arnaud.lecorvaisier@wanadoo.fr>
2023-04-22 17:40:52 -0400

    end

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :movies, only: [:index, :show]
+  resources :lists do
+    resources :bookmarks do
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+      resources :movies, only: [:index, :show]
+
+    end
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
